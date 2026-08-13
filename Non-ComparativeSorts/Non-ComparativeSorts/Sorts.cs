7e51f0bdd9b610fc02d8dbced0df92aaee8978ec
MiloYHua <milo.hua06@gmail.com>
2026-08-12 17:59:19 -0700

    Fixing the organization of non-comparative sorts in the repository.

---
+++ Non-ComparativeSorts/Non-ComparativeSorts/Sorts.cs
@@ -0,0 +1,51 @@
+﻿using System.Data;
+
+namespace Non_ComparativeSorts
+{
+	public class Sorts<T> where T : IKeyable
+	{
+		public void PigeonholeSort(List<T> vals)
+		{
+			Dictionary<int, T> dict = new();
+			int[] buckets = new int[vals.Count + 1];
+
+			foreach (T value in vals)
+			{
+				buckets[value.Key]++;
+				dict.Add(value.Key, value);
+			}
+
+			int index = 0;
+
+			for (int i = 0; i < buckets.Length; i++)
+			{
+				while (buckets[i]-- > 0)
+				{
+					vals[index++] = dict[i];
+				}
+			}
+		}
+	}
+
+	public interface IKeyable
+	{
+		public int Key { get; }
+	}
+}
+
+//uint maxValue = dataset.GetMaxValue();
+//uint[] buckets = new uint[maxValue + 1];
+
+//foreach (int value in dataset)
+//{
+//	buckets[value]++;
+//}
+
+//int dataIndex = 0;
+//for (uint i = 0; i < buckets.Length; i++)
+//{
+//	while (buckets[i]-- > 0)
+//	{
+//		dataset[dataIndex++] = i;
+//	}
+//}
--- Non-ComparativeSorts/SortTests/Tests.cs
+++ Non-ComparativeSorts/SortTests/Tests.cs
@@ -1,16 +1,45 @@
-﻿using Sorts;
+﻿using Non_ComparativeSorts;
+using System.Text;
 
 namespace SortTests
 {
-    public class Tests
-    {
-        [Fact]
-        public void CountingSortTest()
-        {
-            uint[] nums = { 5, 3, 1, 4, 2 };
-            SortCollection.CountingSort(nums);
+	public class Tests
+	{
+		[Theory]
+		[InlineData(123,42345,73451,34656,235435,56834545,762435,762345,643265)]
+		[InlineData(654,24567,48754,53246,46823,52345,6798,2345234,6867,2345)]
+		public void PigeonholeTest(params int[] ints)
+		{
+			foreach(int seed in ints)
+			{
+				string[] names = ["billy", "johnny", "claude", "code", "bobby", "alex", "brandon", "the guy", "john", "guy", "dude", "lolz"];
+				List<Pigeon> pigeons = new List<Pigeon>(names.Length);
+				Random randy = new Random(seed);
+				HashSet<int> seen = new HashSet<int>();
 
-            Assert.Equal(nums, [ 1, 2, 3, 4, 5]);
-        }
-    }
+				while(true) //change
+				{
+					int num = randy.Next(names.Length - 1);
+					if (seen.Contains(num)) continue;
+						seen.Add(num);
+					string name = names[num];
+					pigeons[num] = new Pigeon(num, name);
+				}
+				
+			}
+		}
+	}
+
+	public class Pigeon : IKeyable
+	{
+		public int Key { get; }
+
+		public string Name { get; }
+
+		public Pigeon(int key, string name)
+		{
+			Key = key;
+			Name = name;
+		}
+	}
 }
