756abc96453660aa94b3ccc7b5167eac709d1dce
MiloYHua <milo.hua06@gmail.com>
2026-08-23 16:00:45 -0700

    Worked on implementing the Hash Map. Finishing up Add with Rehash.

--- HashMapADT/HashMapADT/HashMap.cs
+++ HashMapADT/HashMapADT/HashMap.cs
@@ -1,11 +1,13 @@
-﻿namespace HashMapADT
+﻿using System.Runtime.CompilerServices;
+
+namespace HashMapADT
 {
-    public struct Bucket<TKey, TValue>
+    public struct Pair<TKey, TValue>
     {
         public TKey Key { get; }
         public TValue Value { get; }
 
-        public Bucket(TKey key, TValue value)
+        public Pair(TKey key, TValue value)
         {
             Key = key;
             Value = value;
@@ -13,12 +15,71 @@
     }
     public class HashMap<TKey, TValue>
     {
-        LinkedList<Bucket<TKey, TValue>> buckets;
+        readonly LinkedList<Pair<TKey, TValue>>[] buckets;
+        private readonly IEqualityComparer<TKey> keyComparer;
 
-        TValue GetValue(TKey key)
+        public HashMap(IEqualityComparer<TKey> comparer)
+        {
+            keyComparer = comparer;
+
+            /* rest of the constructor goes here */
+        }
+
+        public HashMap()
+            : this(EqualityComparer<TKey>.Default)
+        {
+            /* rest of the constructor goes here */
+        }
+
+        public int ComputeIndex(TKey key)
         {
             int hashCode = key.GetHashCode();
-            int index =
+            return Math.Abs(hashCode % buckets.Length);
+        }
+
+        public int ComputeNewIndex(TKey key)
+        {
+            int hashCode = key.GetHashCode();
+            return Math.Abs(hashCode % buckets.Length * 2);
+        }
+
+        public TValue GetValue(TKey key)
+        {
+            int index = ComputeIndex(key);
+
+            LinkedList<Pair<TKey, TValue>> bucket = buckets[index];
+
+            foreach (Pair<TKey, TValue> pair in bucket)
+            {
+                if (keyComparer.Equals(pair.Key, key)) return pair.Value;
+            }
+            throw new ArgumentException($"Given key: '{key}' is not found.");
+        }
+
+        public void Add(Pair<TKey, TValue> pair)
+        {
+            int index = ComputeIndex(pair.Key);
+
+
+            if (buckets[index] is null || buckets.Length == 0)
+            {
+                LinkedList<Pair<TKey, TValue>> toAdd = [];
+                toAdd.AddFirst(pair);
+
+                buckets[index] = toAdd;
+            }
+            throw new ArgumentException($"Given key: '{pair.Key}' already exists.");
+
+            void Rehash()
+            {
+                LinkedList<Pair<TKey, TValue>>[] newList = [];
+
+                foreach(LinkedList<Pair<TKey, TValue>> bucket in buckets)
+                {
+                    if (bucket is null) continue;
+                    newList[ComputeNewIndex(bucket.)]
+                }
+            }
         }
     }
 }
