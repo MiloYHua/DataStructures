a1393980964e5528c2d482b6e036681634f23969
MiloYHua <milo.hua06@gmail.com>
2026-09-23 19:00:53 -0700

    Found an unrealized bug in the Quick Find Union, still fixing.

---
+++ UnionFind/QuickFindTest/QuickFindTests.cs
@@ -0,0 +1,58 @@
+﻿using UnionFind;
+
+namespace QuickFindTest
+{
+    public class QuickFindTests
+    {
+        [Fact]
+        public void FindAddTest()
+        {
+            Random random = new Random(2);
+            QuickFind<string> quickStringFind = new();
+
+            for (int i = 0; i < 100;)
+            {
+                string randomString = random.Next(1, 10000).ToString();
+                if (quickStringFind.Add(randomString))
+                {
+                    Assert.Equal(i, quickStringFind.Find(randomString));
+                    i++;
+                }
+            }
+        }
+
+        [Fact]
+        public void UnionAreConnectedTest()
+        {
+            Random random = new Random(2);
+            QuickFind<string> quickStringFind = new();
+
+            for (int i = 0; i < 16;)
+            {
+                string randomString = random.Next(1, 10000).ToString();
+                if (quickStringFind.Add(randomString))
+                {
+                    i++;
+                }
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                string a = quickStringFind.Sets[i].First.Value;
+                for (int x = 0; x < 4; x++)
+                {
+                    quickStringFind.Union(a, quickStringFind.Sets[x].First.Value);
+                }
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                string a = quickStringFind.Sets[i].First.Value;
+                for (int x = 0; x < 4; x++)
+                {
+                    Assert.True(quickStringFind.AreConnected(a, quickStringFind.Sets[x].First.Value));
+                }
+            }
+        }
+    }
+}
--- UnionFind/UnionFind/QuickFind.cs
+++ UnionFind/UnionFind/QuickFind.cs
@@ -1,60 +1,85 @@
-﻿namespace UnionFind
+﻿using System.Drawing;
+
+namespace UnionFind
 {
-	interface IUnionFind<T>
-	{
-		// Returns the set that p belongs to
-		int Find(T p);
+    interface IUnionFind<T>
+    {
+        // Returns the set that p belongs to
+        int Find(T p);
 
-		// Connects p and q — returns true if successful, false otherwise
-		bool Union(T p, T q);
+        // Connects p and q — returns true if successful, false otherwise
+        bool Union(T p, T q);
 
-		// Returns true if p and q are connected, false otherwise
-		bool AreConnected(T p, T q);
-	}
+        // Returns true if p and q are connected, false otherwise
+        bool AreConnected(T p, T q);
+    }
 
-	public class QuickFind<T>
-	{
-		LinkedList<T>[] Sets = new LinkedList<T>[4];
+    public class QuickFind<T> : IUnionFind<T>
+    {
+        public LinkedList<T>[] Sets = new LinkedList<T>[4];
 
-		public int Count { get; private set; } = 0;
-		public int Length { get; private set; } = 4;
+        public Dictionary<T, int> Pairs = [];
 
-		public QuickFind()
-		{
-			for(int i = 0; i < Sets.Length; i++)
-			{
-				Sets[i] = [];
-			}
-		}
+        public int Length { get; private set; } = 4;
+        public int Count { get; private set; } = 0;
 
-		public int GetID(T a)
-		{
-			if (Sets.Length < 0) throw new ArgumentException("Bucket was empty");
-			int hashCode = a.GetHashCode();
-			return Math.Abs(hashCode % Sets.Length);
-		}
-		public int GetNewID(T a)
-		{
-			int hashCode = a.GetHashCode();
-			return Math.Abs(hashCode % (Sets.Length * 2));
-		}
-		public void Resize()
-		{
-			LinkedList<T>[] newSets = new LinkedList<T>[Length * 2];
+        public QuickFind()
+        {
+            for (int i = 0; i < Sets.Length; i++)
+            {
+                Sets[i] = [];
+            }
+        }
 
-			foreach(Linked)
-		}
+        public void Resize()
+        {
+            LinkedList<T>[] newSets = new LinkedList<T>[Length * 2];
+            Length *= 2;
 
-		public bool Add(T a)
-		{
-			if (a is null) return false;
-			if (Count == Length) Length *= 2;
+            for (int i = 0; i < Sets.Length; i++)
+            {
+                newSets[i] = Sets[i];
+            }
+            for (int i = Sets.Length; i < Length; i++)
+            {
+                newSets[i] = [];
+            }
+            Sets = newSets;
+        }
 
+        public bool Add(T a)
+        {
+            if (a is null || Pairs.Keys.Contains(a)) return false;
+            if (Count == Length) Resize();
 
-			Sets[Count].AddFirst(a);
-			Count++;
+            Sets[Count].AddFirst(a);
+            Pairs.Add(a, Count);
+            Count++;
 
-			return true;
-		}
-	}
+            return true;
+        }
+
+        public int Find(T p) => Pairs[p];
+
+        public bool Union(T p, T q)
+        {
+            if (p is null || q is null) return false;
+
+            LinkedList<T> setP = Sets[Pairs[p]];
+            foreach (T item in setP)
+            {
+                Pairs[item] = Pairs[p];
+            }
+
+            return true;
+        }
+
+        public bool AreConnected(T p, T q)
+        {+
+            if (p is null || q is null) return false;
+
+            if (Pairs[q] == Pairs[p]) return true;
+            return false;
+        }
+    }
 }
